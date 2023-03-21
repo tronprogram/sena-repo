6d7a86e74c6c13148a0a4f8e167bb20c5ff9bee4
tronprogram <bassgamer1234@outlook.es>
2023-03-21 08:45:39 -0500

    new file:   act2/app.js 	new file:   act2/index.html 	new file:   act2/styles.css

---
+++ act2/app.js
@@ -0,0 +1,6 @@
+function sumar(){
+    let num1=parseInt(document.getElementById('num1').value)
+    let num2=parseInt(document.getElementById('num2').value)
+    let resultado=num1+num2
+    document.getElementById('resultado').textContent=resultado
+}
